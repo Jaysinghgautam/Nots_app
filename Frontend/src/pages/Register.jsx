8f0d45341ad046134a3ef8d43c373504160ad55e
Jaysinghgautam <gjaysingh53@gmail.com>
2025-08-13 15:37:25 +0530

    updated

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { post } from '../services/ApiEndPoint'
 import toast from 'react-hot-toast'
+import api from '../services'
  
 
 export default function Register() {
